a03cbaac3b50646c2458f7bf3987537a70dcd62f
ryanvanderstelt <ryan@vstelt.com>
2026-01-30 22:12:30 -0600

    basic app with fastAPI instead

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React dev server
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
