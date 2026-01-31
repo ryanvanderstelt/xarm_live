80f98db947ead81d7be441950bc4cfceb709af6f
ryanvanderstelt <ryan@vstelt.com>
2026-01-30 23:26:30 -0600

    basic video backend, untested

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+camera = cv2.VideoCapture(0)  # Use 0 for the default camera
 
 app.add_middleware(
     CORSMiddleware,
@@ -12,14 +13,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# templates = Jinja2Templates(directory="templates")
 
 # Function to generate frames from the camera
 def generate_frames():
-    camera = cv2.VideoCapture(0)  # Use 0 for the default camera
-    if not camera.isOpened():
-        raise RuntimeError("Could not start camera.")
-
     while True:
         success, frame = camera.read()
         if not success:
@@ -32,14 +28,6 @@ def generate_frames():
             # Yield the frame in the multipart format
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-        
-        # Optional: Add a small delay
-        time.sleep(0.03)
-
-# @app.get("/")
-# def index(request: Request):
-#     # Serve the HTML template
-#     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/video_feed")
 def video_feed():
